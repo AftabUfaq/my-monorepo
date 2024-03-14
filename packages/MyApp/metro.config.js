e1716c840314c9841c67b3444559bf211d0353a1
Kasim Ali <kasim.ali@and.digital>
2024-03-14 16:44:43 +0000

    Initial commit

---
+++ packages/MyApp/metro.config.js
@@ -0,0 +1,29 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+
+// Retrieve the default configuration
+const defaultConfig = getDefaultConfig(__dirname);
+
+// Define additional configuration to merge
+const extraConfig = {
+  // Custom resolver options
+  resolver: {
+    // Define additional module resolutions
+    extraNodeModules: new Proxy({}, {
+      get: (target, name) => {
+        // Redirects dependencies referenced from the shared package to local node_modules
+        if (target.hasOwnProperty(name)) {
+          return target[name];
+        }
+        return path.join(__dirname, `node_modules/${name}`);
+      },
+    }),
+  },
+  // Additional folders to be watched by the Metro bundler
+  watchFolders: [
+    path.resolve(__dirname, '../shared'), // Adjust this path to where your shared folder is located relative to this config file
+  ],
+};
+
+// Merge the default config with your additional configuration
+module.exports = mergeConfig(defaultConfig, extraConfig);
